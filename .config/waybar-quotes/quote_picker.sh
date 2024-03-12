9546ec68f86e7aed40795101b31f87d17c60f2e9
PigmanBruh <87263998+PigmanBruh@users.noreply.github.com>
2024-03-12 22:39:52 +0700

    Delete .config to rearrange stuff

--- .config/waybar-quotes/quote_picker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PICKED=$((1 + $RANDOM % $(sed -n '$=' ~/.config/waybar-quotes/quotes.txt)))
-
-echo $(head -$PICKED ~/.config/waybar-quotes/quotes.txt | tail +$PICKED) > ~/.config/waybar-quotes/picked_quote.txt
